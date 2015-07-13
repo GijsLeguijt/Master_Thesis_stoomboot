e323dbd3a71b53d22c764c3b1598ff386ad3277c
acolijn <auke.pieter.colijn@gmail.com>
2015-07-13 22:03:50 +0200

    runsim.py changing ....

--- macros/runsim.py
+++ macros/runsim.py
@@ -6,7 +6,6 @@
 # Run GEANT4 simulations for the Modulation setup
 #
 #
-#
 # A.P. Colijn - colijn@nikhef.nl
 #
 
@@ -19,24 +18,23 @@ output_basedir = "/data/atlas/users/acolijn/Modulation/simulation"
 # where should the scripts for running go?
 run_dir = output_basedir + "/macros"
 
+# ROOT VERSION
+ROOT_version = "5.34.25-x86_64-slc6-gcc48-opt"
+# GEANT4 VERSION
+GEANT4_version = "10.1.p02"
+
+
 # temporary variables
 ran_seed        = 12345
 source_name     = "Ti44"
-output_basename = source_name + "_"+ str(ran_seed)
-
-output_root     = output_basename + ".root"
-
-preinit_script = ''
-run_script = ''
 
 #--------------------------------------------------------------------------------------------
-def make_preinit_script():
+def make_G4preinit_script():
     #
     # make the preinit_script
     #
-    print('make_preinit_script:: start generating script')
-    global preinit_script
-    preinit_script = run_dir+'/preinit_'+output_basename+'.mac'
+    print('make_preinit_script:: macro ='+preinit_script)
+    
     fout = open(preinit_script,'w')
     
     fout.write('# GEANT4 - G4simu pre_init macro (AUTOMATICALLY GENERATED) \n')
@@ -53,13 +51,12 @@ def make_preinit_script():
     
     fout.close()
 #--------------------------------------------------------------------------------------------
-def make_run_script():
+def make_G4run_script():
     #
     # make the run_script
     #
-    print('make_run_script:: start generating script')
-    global run_script
-    run_script = run_dir+'/run_'+output_basename+'.mac'
+    print('make_run_script:: macro ='+run_script)
+
     fout = open(run_script,'w')
     
     fout.write('# GEANT4 - G4simu run macro (AUTOMATICALLY GENERATED) \n')
@@ -68,7 +65,7 @@ def make_run_script():
     fout.write('/event/verbose    0\n')
     fout.write('/tracking/verbose 0\n')
     
-    fout.write('/run/random/setRandomSeed 12346\n')
+    fout.write('/run/random/setRandomSeed '+ran_seed+'\n')
     
     fout.write('/gps/pos/type    Volume\n')
     fout.write('/gps/pos/shape   Sphere\n')
@@ -92,12 +89,53 @@ def make_run_script():
     
     fout.close()
 #--------------------------------------------------------------------------------------------
+def make_shell_script():
+    #
+    # make the shell script for running on stoomboot
+    #
+
+    print('make_run_script:: macro ='+run_script)
+    fout = open(run_script,'w')
+    fout.write('#!/bin/sh\n')
+
+    fout.write('# setup root\n')
+    fout.write('source /project/atlas/nikhef/cvmfs/setup.csh\n')
+    fout.write('setupATLAS\n')
+    fout.write('localSetupROOT '+ROOT_version+'\n')
+
+    fout.write('# setup GEANT4\n')
+    fout.write('setenv BASEDIR `pwd`\n')
+    fout.write('source /cvmfs/geant4.cern.ch/etc/login.csh\n')
+    fout.write('cd /cvmfs/geant4.cern.ch/geant4/'+GEANT4_version+'/x86_64-slc6-gcc48-opt/bin/\n')
+    fout.write('source geant4.csh\n')
+    fout.write('cd ${BASEDIR} \n')
+
+    fout.write(simulation_basedir+'/../modusim-build/G4simu -p '+preinit_script+' -f '+run_script+' -n '+str(numberOfEvents))
+
+    fout.close()
+#--------------------------------------------------------------------------------------------
 # MAIN CODE
 
-make_preinit_script()
+numberOfEvents = 123
 
-make_run_script()
+# output structure and files
+output_basename = source_name + "_"+ str(ran_seed)
+output_root     = output_basename + ".root"
+
+# macros and scripts
+preinit_script = run_dir+'/preinit_'+output_basename+'.mac'
+run_script     = run_dir+'/run_'+output_basename+'.mac'
+shell_script   = run_dir+'/simu_'+output_basename+'.sh'
 
 
-print('PREINIT = '+preinit_script)
-print('RUN     = '+run_script)
+# generate the GEANT4 preinit script
+make_G4preinit_script()
+
+# generate the GEANT4 run script
+make_G4run_script()
+
+# generate run shell script
+make_shell_script()
+
+
+
