bfb3f438e36b4eeaab5bcbe33e37c9676b2fdb56
acolijn <auke.pieter.colijn@gmail.com>
2019-01-06 17:45:31 +0100

    minor change

--- setup_stoomboot.sh
+++ setup_stoomboot.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+
+#gcc
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_94/gcc/8.2.0/x86_64-centos7/setup.sh
+#cmake
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_94/CMake/3.11.1/x86_64-centos7-gcc8-opt/CMake-env.sh
+#geant4
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_94/Geant4/10.04.p02/x86_64-centos7-gcc8-opt/bin/geant4.sh
+#ROOT
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_94/ROOT/6.14.04/x86_64-centos7-gcc8-opt/ROOT-env.sh
 
 # end of config
